c39fa34148db88a822fd7f5e4bb7a9f165d72d4d
VASU CHAND <vasuchand9@gmail.com>
2026-02-11 19:50:24 +0530

    add

---
+++ kadane/majorityoptimize.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int arr[5]={1,2,2,1,1};
+    sort(arr,arr + 5);
+    int freq=1;
+    for(int i=1; i<5; i++){
+        if(arr[i]==arr[i-1]){
+            freq++;
+        }
+        else{
+            freq=1;
+        }
+        if(freq>3){
+            cout << arr[i] << endl;
+            break;
+        }
+    }
+    return 0;
+
+
+
+}
