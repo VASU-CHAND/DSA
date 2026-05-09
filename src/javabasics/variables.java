4c3017a3bb47cec98d72aefe90d15fd4d73c1f01
VASU CHAND <vasuchand9@gmail.com>
2026-05-09 16:14:58 +0530

    ad

--- src/javabasics/variables.java
+++ src/javabasics/variables.java
@@ -1,16 +1,5 @@
 public class variables {
-    public static void main(String args[]){
-     int a =10;
-     int b =5;
-     System.out.println(a);
-     System.out.println(b);
-     String name = "tony stark";
-     System.out.println(name);
-     a=50;
-     System.out.println(a);
-     int sum = a+b;
-     System.out.println(sum);
-
-
+    public static void main(String[] args) {
+        System.out.println("hello ");
     }
 }
