ed1c9df5ccb17e44d83eb6323ca755899fe05f3f
VASU CHAND <vasuchand9@gmail.com>
2026-05-09 17:10:36 +0530

    add

---
+++ src/javabasics/input.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class input{
+    public static void main (String[] args){
+        // scanner classs we need to make
+        // ye function space le baad ka stor ni krta hai
+       Scanner sc = new Scanner(System.in);
+      // String input= sc.next();
+      // System.out.println(input);
+       // psce ke baad ka bhi aghar stor karwana ho tb aise karinge
+       String name= sc.nextLine();
+       System.out.println(name);
+
+    }
+}
