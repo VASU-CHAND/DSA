390eb17fbffd7a5bd8758cfad880488dc2a01a53
VASU CHAND <vasuchand9@gmail.com>
2026-05-14 23:41:00 +0530

    add

---
+++ src/patterns/star.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class star {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+     int n = sc.nextInt();
+
+    for( int line=0; line<n; line++ ){
+        for(int star=0; star<=line; star++){
+             System.out.print("*");
+        }
+        System.err.println();
+    }
+}
+}
