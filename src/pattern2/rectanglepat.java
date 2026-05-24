85ac692a666f43521b399ce858c01f1ed6139f5d
VASU CHAND <vasuchand9@gmail.com>
2026-05-24 11:33:42 +0530

    add

---
+++ src/pattern2/rectanglepat.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class rectanglepat {
+    
+public static void hollowrec(int rows , int cols){
+    //outer loop
+    for(int i=1; i<=rows; i++){
+        // innner cols
+        for(int j=1; j<=cols; j++){
+            if(i==1|| i==rows || j==1|| j==cols){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+                 }
+             System.out.println();
+
+           }
+}
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+       hollowrec(3, 7);
+}
+}
