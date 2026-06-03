0e0426ed0368ff39cc6b807574c65b249f712b54
VASU CHAND <vasuchand9@gmail.com>
2026-06-03 09:14:26 +0530

    add

---
+++ src/arrays/binarysearch.java
@@ -0,0 +1,28 @@
+public class binarysearch {
+    public static int search(int numbers[] , int key){
+        int start=0 ;
+        int end= numbers.length -1;
+        while(start<end){
+            int mid= (start +end)/2;
+        
+        if(numbers[mid]==key){
+            return mid;
+        }
+        if(numbers[mid]<key){
+            start=mid+1;
+        }
+        else{
+            end= mid-1;
+        }
+         }
+         return -1;
+        }
+        public static void main(String[] args) {
+            int num[]= {2,4,6,8,10,12,14};
+            int key =12;
+            int index= search(num ,key);
+            System.out.println(index);
+        }
+    }
+     
+
