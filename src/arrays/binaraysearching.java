b80c6d3d439de868e1e08958f2269fff1ee71139
VASU CHAND <vasuchand9@gmail.com>
2026-06-05 16:38:12 +0530

    ADD

--- src/arrays/binaraysearching.java
+++ src/arrays/binaraysearching.java
@@ -2,7 +2,9 @@ public class binaraysearching {
     public static int search(int arr[] , int key){
       int start=0;
       int end = arr.length -1;
+      while(end<=start){}
       
+
     }
     public static void main(String[] args) {
         
