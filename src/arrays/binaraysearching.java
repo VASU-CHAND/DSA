1012097f97b877cac4822d5f96a8ea5d24b29171
VASU CHAND <vasuchand9@gmail.com>
2026-06-06 07:41:56 +0530

    add

--- src/arrays/binaraysearching.java
+++ src/arrays/binaraysearching.java
@@ -14,10 +14,12 @@ public class binaraysearching {
         start= mid+1;
        }
       }
-      
+      return -1;
     }
     public static void main(String[] args) {
-      int num[]={1,5,8,9,10};
+      int num[]={1,5,8,9,10 ,22,45,48,99};
+      int keyindex= search(num ,22 );
+      System.err.println(keyindex);
         
     }
 }
