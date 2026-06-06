4c2aa73872653f9d30a3cd18223b7c30ba480e5d
VASU CHAND <vasuchand9@gmail.com>
2026-06-06 07:51:58 +0530

    add

--- src/arrays/binaraysearching.java
+++ src/arrays/binaraysearching.java
@@ -2,12 +2,12 @@ public class binaraysearching {
     public static int search(int arr[] , int key){
       int start=0;
       int end = arr.length -1;
-      while(end<=start){
+      while(end>=start){
         int mid = (start + end )/2;
-        if(mid==key){
+        if(arr[mid]==key){
           return mid;
         }
-       if(mid>key){
+       if(arr[mid]>key){
         end= mid -1;
        }
        else{
@@ -17,7 +17,7 @@ public class binaraysearching {
       return -1;
     }
     public static void main(String[] args) {
-      int num[]={1,5,8,9,10 ,22,45,48,99};
+      int num[]={1,5,8,9,10,22,45,48,99};
       int keyindex= search(num ,22 );
       System.err.println(keyindex);
         
