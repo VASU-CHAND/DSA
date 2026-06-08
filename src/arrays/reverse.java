49a9de29eba222086dbaa53c78751a8ba74f6edf
VASU CHAND <vasuchand9@gmail.com>
2026-06-08 08:04:54 +0530

    add

--- src/arrays/reverse.java
+++ src/arrays/reverse.java
@@ -13,7 +13,9 @@ public class reverse {
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5,6,7};
         reverse(arr);
+        System.err.println("reversed arr");
         for (int i = 0; i < arr.length; i++) {
+            
             System.out.println(arr[i]);
         }
     }
