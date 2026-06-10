44ec35a830f4306da891a9cab157fa479aa11460
VASU CHAND <vasuchand9@gmail.com>
2026-06-10 08:50:24 +0530

    add

--- src/arrays/subarray.java
+++ src/arrays/subarray.java
@@ -1,3 +1,18 @@
 public class subarray {
+ public static void subarr(int arr[]){
+          for (int i = 0; i < arr.length; i++) {
+                for( int j=i+1; j<=arr.length; j++ ){
+                        for(int k=i;k<j;k++ ){
+                        System.err.print(arr[k] +);
+                          }
+                          System.err.print(" ");
+                  }
+                          System.err.println("");
 
+            }
+ }
+ public static void main(String[] args) {
+     int numbers[]={2,4,6,8,10};
+      subarr(numbers);
+ }
 }
