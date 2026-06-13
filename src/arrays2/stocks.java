efef03fadde7b94cbaac0daca18d357a57d17bb4
VASU CHAND <vasuchand9@gmail.com>
2026-06-13 08:42:16 +0530

    add

--- src/arrays2/stocks.java
+++ src/arrays2/stocks.java
@@ -1,3 +1,25 @@
 public class stocks {
-    
+    public static int buysellstock(int arr[]){
+      int buyprice= Integer.MAX_VALUE;
+      int maxprofit=0;
+
+    for (int i = 0; i < arr.length; i++) {
+        if(buyprice<arr[i]){
+            int profit=arr[i]-buyprice;
+            maxprofit=Math.max(maxprofit,profit);
+        }
+        else{
+            buyprice=arr[i];
+        }
+    }
+
+
+
+        return maxprofit;
+    }
+    public static void main(String[] args) {
+        int prices[]={7,1,5,3,6,4};
+        System.err.println(buysellstock(prices));
+
+    }
 }
