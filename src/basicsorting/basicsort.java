443607fc3064663a61b4c118b888a6707a1b7f97
VASU CHAND <vasuchand9@gmail.com>
2026-06-15 15:31:38 +0530

    add

--- src/basicsorting/basicsort.java
+++ src/basicsorting/basicsort.java
@@ -3,9 +3,22 @@
 
 public class basicsort {
     public static void bubblesort(int arr[]){
-        
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j <arr.length-1-i; j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp=0;
+                     temp= arr[j+1];
+                     arr[j+1]=arr[j];
+                     arr[j]=temp;
+                }
+            }
+        }
     }
     public static void main(String[] args) {
-        
+        int arr[]={5,4,1,3,2};
+        bubblesort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
