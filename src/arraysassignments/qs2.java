709a7f741dea9339af3ea53f099a873ac4653d28
VASU CHAND <vasuchand9@gmail.com>
2026-06-15 15:06:12 +0530

    add

---
+++ src/arraysassignments/qs2.java
@@ -0,0 +1,8 @@
+public class qs2 {
+    public static int target(int arr[]){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/basicsorting/basicsort.java
@@ -0,0 +1,11 @@
+
+
+
+public class basicsort {
+    public static void bubblesort(int arr[]){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
