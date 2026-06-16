efa2fac332d282cb32bf141a2603990dc9c47be4
VASU CHAND <vasuchand9@gmail.com>
2026-06-16 08:02:19 +0530

    add

---
+++ src/basicsorting/inbuiltsort.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class inbuiltsort {
+    //Arrays.sort(arr)
+    //Arrays.sort(arr,start i,ending i)
+    //O(nlogn)
+    public static void main(String[] args) {
+                         int arr[]={5,4,1,3,2};
+                         Arrays.sort(arr);
+                          for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+                                        }
+    }
+  
