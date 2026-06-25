6fa5976419d0af97fbc619e76a8c9f0f36a2bd91
VASU CHAND <vasuchand9@gmail.com>
2026-06-25 08:25:21 +0530

    add

--- src/arrays2/kadane1.java
+++ src/arrays2/kadane1.java
@@ -4,12 +4,20 @@ public class kadane1 {
          int cs=0;
          for (int i = 0; i < arr.length; i++) {
              cs= cs+arr[i];
+             if(cs<0){
+                cs=0;
+             }
+            maxsum= Math.max(cs, maxsum);
          }
              
 
         return maxsum;
     }
     public static void main(String[] args) {
+        int arr[]={-2,-3,4,-1,-2,1,5,-3};
+        int maxsum=msum(arr);
+         System.err.println(maxsum);
+            
         
     }
 }
