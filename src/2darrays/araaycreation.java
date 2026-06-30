9b5c527a3a53db6c0bc17e0771d992a4937ffb7f
VASU CHAND <vasuchand9@gmail.com>
2026-06-30 16:29:18 +0530

    add

--- src/2darrays/araaycreation.java
+++ src/2darrays/araaycreation.java
@@ -1,8 +1,19 @@
 
+import java.util.Scanner;
+
+
 public class araaycreation {
 public static void main(String args[]){
     int matrix[][]=new int[3][3];
-    
+    int n=3;
+    int m=3;
+    Scanner sc = new Scanner(System.in);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j< m; j++) {
+            System.err.println("enter the element "+(i)+"'th row and "+(j)+"'th coloumn" );
+            matrix[i][j]=sc.nextInt();
+        }
+    }
 }
     
 }
