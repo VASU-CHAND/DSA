2ca0bff80c7a6d1dfd5c7796457768dd8a238ef5
VASU CHAND <vasuchand9@gmail.com>
2026-07-02 13:50:22 +0530

    add

---
+++ src/2darrays/sortedarrsearch.java
@@ -0,0 +1,18 @@
+public class sortedarrsearch {
+    
+public static int search(int arr[][], int key){
+    int start=0;
+    int end= arr.length-1;
+    int mid1 = start +(end -start)/2;
+    if(arr[mid][])
+for (int i = 0; i < 10; i++) {
+    
+}
+
+    return 0;
+}
+
+
+
+
+}
---
+++ src/Strings/stringfns.java
@@ -0,0 +1,14 @@
+
+
+public class stringfns {
+    public static void main(String[] args) {
+        // compare
+   String str = "vasu";
+   String str2= "Vasu";
+   String str3 = new String("vasu");
+
+        //substring
+        String str4="hellovasu";
+  System.err.println(str4.substring(2, 6));
+    }
+}
